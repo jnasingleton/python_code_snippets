3b3ca631a3a3484ce72f56e470dcb6922d059d7d
Jamie Singleton <jnasingleton@gmail.com>
2018-10-04 12:04:39 -0400

    Update extract_db_structure.py

--- extract_db_structure.py
+++ extract_db_structure.py
@@ -30,18 +30,10 @@ def extract_db_structure(csv_file, db_file, db_pwd):
     curs_tables.close()
     conn.close()
             
-db1_file = os.path.join('D:' + os.sep,'My Documents','BMS','BMS_ICP','BMS_ICP_2017','Data','db_Data.mdb')
+db1_file = ''
 db1_pwd = ''
-db2_file = os.path.join('D:' + os.sep,'My Documents','BMS','BMS_ICP','BMS_ICP_2017','Data','db_Temp.mdb')
-db2_pwd = ''
-db3_file = os.path.join('D:' + os.sep,'My Documents','BMS','BMS_ICP','BMS_ICP_2017','Data','db_Temp2.mdb')
-db3_pwd = ''
-db4_file = os.path.join('D:' + os.sep,'My Documents','BMS','BMS_ICP','BMS_ICP_2017','Data','db_Audit.mdb')
-db4_pwd = 'bmsicp2010'
-db5_file = os.path.join('D:' + os.sep,'My Documents','BMS','BMS_ICP','BMS_ICP_2017','Config','db_Config.mdb')
-db5_pwd = 'bmsicp2010'
 
-db_files = [(db1_file, db1_pwd), (db2_file, db2_pwd), (db3_file, db3_pwd), (db4_file, db4_pwd), (db5_file, db5_pwd)]
+db_files = [(db1_file, db1_pwd)]
 for db in db_files:
     db_file = db[0]
     db_pwd = db[1]
